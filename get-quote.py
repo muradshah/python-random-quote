632d08a72165270bb6262edb428bb33c75a074c8
muradshah <murad.hadi@gmail.com>
2022-07-05 03:49:26 +0500

    adding qoute programingly

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,25 @@
 import random
+from re import A
+from tkinter import W
 
 
 def hello_world():
+
+  f = open("quotes.txt", 'a')
+  L = "This is programaticly added qoute \n"
+  f.write(L)
+  f.close()
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
+  last = 21
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print (quotes[rnd1], end="")
+  print (quotes[rnd2])
 
 if __name__== "__main__":
   hello_world()
